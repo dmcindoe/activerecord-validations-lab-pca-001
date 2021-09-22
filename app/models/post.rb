31d19297631976a05bf35013806adefe3006a218
Andrew McIndoe <amcindoe@powerhrg.com>
2021-09-22 19:39:18 -0400

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: ["Fiction", "Non-Fiction"]
+  validate :clickbait
+
+  def clickbait
+    bait = ["Won't Believe", "Secret", "Top", "Guess"]
+    errors.add(:title, "Invalid title name!") if title && bait.none? { |word| title.include?(word) }
+  end 
 end
